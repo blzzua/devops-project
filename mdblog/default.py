19e47b12f2a826456d921adffb3b3d9f3041cce7
blzzua <blzzua@github.com>
2023-02-27 13:17:21 +0200

    postgres as database

--- mdblog/default.py
+++ mdblog/default.py
@@ -1,3 +1,4 @@
+import os
 
 SECRET_KEY = b'\x8eJ|P7\x8c\xe6X\xb3\x9c\xaf\x17C\xbaz\x17\xbb\xc81`_\xe3\xac\xc2'
 # Login
@@ -6,7 +7,6 @@ USERNAME = "admin"
 PASSWORD = "admin"
 
 #SQL
-
-SQLALCHEMY_DATABASE_URI = "sqlite:///./blog.db"
+SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', 'sqlite:///./blog.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 DEBUG = True
